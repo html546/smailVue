ecee4e9ed89c29f8a567bc9492af83d74ebc1640
chengjiang <15712859926@163.com>
2018-11-08 08:16:04 +0800

    first commit

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search-bar">
+        <van-row>
+            <van-col span="3">icon</van-col>
+            <van-col span="16">search-input</van-col>
+            <van-col span="5">button</van-col>
+        </van-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {
+            msg:'Shopping Mall'
+        }
+    }
+}
+</script>
+
+<style lang="" scoped>
+.search-bar{
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+}
+</style>
